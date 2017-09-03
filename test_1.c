5284bbcb1708419a5b26d32632e97bb3101eba81
Adrian <adrian.carstea@gmail.com>
2017-09-03 09:25:43 +0300

    modify test_1.c

--- test_1.c
+++ test_1.c
@@ -4,9 +4,20 @@
 #include <limits.h>
 #include <stdint.h>
 
+void didilea(char *didi){
+	printf("%d\n",didi[1]);
+	didi[2] = 45;
+}
+
 int main(int argc, char *argv[])
 {
-	double a = 0.1;
-	printf("%.20f\n",a);
-
+	//char didi[] = {1,2,3};
+	char *didi = (char *)malloc(sizeof(char)*3);
+	didi[0] = 1;
+	didi[1] = 2;
+	didi[2] = 3;
+	printf("%p\n%p\n",didi,&didi);
+	didilea(didi);
+	printf("%d\n",didi[2]);
+	free(didi);
 }
