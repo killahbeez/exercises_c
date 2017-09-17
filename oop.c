4e711aff5aa68ef2081fd38aba41842cc7377757
Adrian <adrian.carstea@gmail.com>
2017-09-17 18:20:59 +0300

    modified oop

--- oop.c
+++ oop.c
@@ -3,36 +3,31 @@
 #include <stdint.h>
 
 typedef struct didi {
-	int a;
-	int b;
+	int result;
 	void (*meth)(struct didi* this,int a, int b);
 } didi_t;
 
-void method_1(struct didi *,int,int);
-void method_2(struct didi *,int,int);
+void adding(struct didi *,int,int);
+void multiply(struct didi *,int,int);
 
 int main(){
 	didi_t obj;
-	obj.a = 3;
-	obj.b = 4;
-	obj.meth = method_1;
-	(obj.meth)(&obj,obj.a,obj.b);
-	printf("a: %d\n",obj.a);
-	printf("b: %d\n",obj.b);
-	obj.meth = method_2;
-	(obj.meth)(&obj,obj.a,obj.b);
-	printf("a: %d\n",obj.a);
-	printf("b: %d\n",obj.b);
+	
+	obj.meth = adding;
+	obj.meth(&obj,2,3);
+	printf("adding result: %d\n",obj.result);
+
+	obj.meth = multiply;
+	obj.meth(&obj,2,3);
+	printf("multiply result: %d\n",obj.result);
 }
 
-void method_1(didi_t *this,int a, int b){
-	this->a = 31;
-	this->b = 41;
+void adding(didi_t *this,int a, int b){
+	this->result = a + b;
 }
 
-void method_2(didi_t *this,int a, int b){
-	this->a = 32;
-	this->b = 42;
+void multiply(didi_t *this,int a, int b){
+	this->result = a * b;
 }
 
 
