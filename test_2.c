18586f9207318197db03e7d61f51dac15fffa52b
Adrian <adrian.carstea@gmail.com>
2017-10-10 13:50:58 +0300

    check if union vars points to the same address

--- test_2.c
+++ test_2.c
@@ -26,6 +26,9 @@ int main(){
 	union {
 		uint8_t a;
 		float b;
+		uint8_t c;
+		uint8_t d;
+		uint8_t e;
 	} un;
 
 	un.b = 345.65;
@@ -36,13 +39,39 @@ int main(){
 	printf("%d::%x\n",*(&un.a+2),*(&un.a+2));
 	printf("%d::%x\n",*(&un.a+3),*(&un.a+3));
 	uint32_t gr1 = 61455;
-	uint8_t *gr = &gr1;
+	uint8_t *gr = (uint8_t *) &gr1;
 	printf("%x\n",gr1);
 	uint8_t *f = gr + 1;
 	f[0] = 0xff;
 	f[1] = 0xff;
 	f[2] = 0xff;
 	printf("%x\n",gr1);
+	printf("------------\n");
+	printf("un.a address: %p\n",&(un.a));
+	printf("un.b address: %p\n",&(un.b));
+	printf("un.c address: %p\n",&(un.c));
+	printf("un.d address: %p\n",&(un.d));
+	printf("un.e address: %p\n",&(un.e));
+	printf("un.a: %d\n",*(&un.a));
+	printf("un.b: %f\n",*(&un.b));
+
+	typedef struct {
+		uint8_t a;
+		uint8_t b;
+		uint8_t c;
+		uint8_t d;
+		uint8_t e;
+	} st_t;
+	st_t st = {.a = 23, .b = 54};
+	printf("st.a address: %p\n",&(st.a));
+	printf("st.b address: %p\n",&(st.b));
+	printf("st.c address: %p\n",&(st.c));
+	printf("st.d address: %p\n",&(st.d));
+	printf("st.e address: %p\n",&(st.e));
+	printf("st.a: %d\n",st.a);
+	printf("st.b: %d\n",st.b);
+	printf("st.c: %d\n",st.c);
+	printf("st.d: %d\n",st.d);
 }
 
 void method_1(didi_t *this,int a, int b){
